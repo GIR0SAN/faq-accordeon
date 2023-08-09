e4714da3e50ed75bc243dc060543d766642417fc
GiroSan <girosan86@gmail.com>
2023-08-09 13:08:52 -0300

    JS finished

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ const accordion = document.querySelector('[data-js="accordion"]')
 accordion.addEventListener('click', e =>{
     const accordionHeaderId = e.target.dataset.accordionHeader
     
-    const clickedAccordionHeader = document.querySelector(`[data-accordion-header="${accordionHeaderId}"]`)
+    const clickedAccordionHeader = 
+        document.querySelector(`[data-accordion-header="${accordionHeaderId}"]`)
 
-    const accordionItemToBeOpened = document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
+    const accordionItemToBeOpened = 
+        document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
+    const accordionHeaderToBeClosed = Array
+        .from(document.querySelectorAll('[data-js="accordion-header"]'))
+        .filter(accordionHeader => accordionHeader !== clickedAccordionHeader)
+        .find(accordionHeader => accordionHeader.classList.contains('active'))
+
+        if(accordionHeaderToBeClosed){
+
+           const accordionHeaderId =  accordionHeaderToBeClosed.dataset.accordionHeader
+           const accordionBodyToBeClosed = document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
+
+           accordionHeaderToBeClosed.classList.remove('active')
+           accordionBodyToBeClosed.classList.remove('active')
+            
+        }
 
     clickedAccordionHeader.classList.toggle('active')
     accordionItemToBeOpened.classList.toggle('active')
-
 })
